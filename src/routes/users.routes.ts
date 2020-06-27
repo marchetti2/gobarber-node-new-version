0a3c7cb5458f4508b0e4299db22fc34e0ba52140
Mario Luiz <marchetti2@gmail.com>
2020-06-27 10:50:21 -0300

    upload images

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,15 @@
-import { Router } from "express";
+import { Router, request, response } from "express";
 import { getRepository } from "typeorm";
+import multer from "multer";
 
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 import User from "../models/Users";
 import CreateUserService from "../services/CreateUserService";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+import uploadConfig from "../config/upload";
 
 const usersRoute = Router();
-const createAppointmentService = new CreateUserService();
+const upload = multer(uploadConfig);
 
 usersRoute.get("/", async (_, response) => {
   const appointmentRepository = getRepository(User);
@@ -15,6 +19,7 @@ usersRoute.get("/", async (_, response) => {
 
 usersRoute.post("/", async (request, response) => {
   try {
+    const createAppointmentService = new CreateUserService();
     const { name, email, password }: User = request.body;
     const user = await createAppointmentService.execute({
       name,
@@ -29,5 +34,27 @@ usersRoute.post("/", async (request, response) => {
     return response.json({ error: err.message });
   }
 });
+
+usersRoute.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.json({ error: err.message });
+    }
+  }
+);
 
 export default usersRoute;

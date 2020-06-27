a738bacdf65acc46d91517f78b82c438894664ce
Mário Luiz <marchetti2@gmail.com>
2020-06-27 10:33:47 -0300

    Revert "Teste"

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -15,8 +15,6 @@ class User {
   email: string;
   @Column()
   password: string;
-  @Column()
-  avatar: string;
   @CreateDateColumn()
   created_at: Date;
   @CreateDateColumn()
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import "reflect-metadata";
-
-import upload from "./config/upload";
 import routes from "./routes";
 
 import "./database";
 
 const app = express();
 app.use(express.json());
-app.use("/files", express.static(upload.directory));
+
 app.use(routes);
 
 app.listen(3333, () => {
